609a95a44627f69e9bf9d7b6506be3fc464a765f
mohitcse8@gmail.com <mohitcse8@gmail.com>
2020-06-25 11:38:41 +0530

    Commit

---
+++ src/main/java/com/emm/EmmApplication.java
@@ -0,0 +1,13 @@
+package com.emm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emm/EmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
